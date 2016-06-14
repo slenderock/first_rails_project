7a988128e38556a5b40dde7f0cad2c92992bdc95
slenderock <sergiy20101995@ukr.net>
2016-06-14 22:50:42 +0300

    Add self search metod, delete true, add age calculating

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,13 +13,21 @@ class User < ActiveRecord::Base
 
   private
 
+  def self.search (full_name)                 #I will change this method, for less monkey code but now I'll move up
+    splited_name = full_name.split(' ')
+    find_by_first_name_and_last_name(splited_name.first, splited_name.last)
+  end
+
   def check_name_presence
     self.active = first_name.present? && last_name.present?
+  end
 
-    true
+  def age_calculate
+    now = Time.now.utc
+    now.year - birthday.year - (birthday.to_time.change(:year => now.year) > now ? 1 : 0)
   end
 
   def age_checking
-    errors.add(:birthday, "U are too small, baby") if birthday && (Time.now.utc.year - birthday.year) < 15
+    errors.add(:birthday, "U are too small, baby") if birthday && age_calculate < 15
   end
 end
