8658c312862a009f98fa48f6e4befcb653c73f4b
slenderock <sergiy20101995@ukr.net>
2016-06-14 19:50:00 +0300

    fix error with birthday validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :email, :birthday, :active
+
   before_save :check_name_presence
-  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
-  validates :email, :uniqueness => true
-  validate  :age_checking
+
   scope :active_user, ->  { where active: true }
   scope :oldfag,      ->  { active_user.where('birthday <= ?', Time.now - 21.year) }
   scope :search,      ->  (first_name, last_name)  { where(first_name: first_name, last_name: last_name)}
+
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates :email, :uniqueness => true
+  validates :birthday, :presence => true
+  validate :age_checking
 
   private
 
@@ -17,7 +21,6 @@ class User < ActiveRecord::Base
   end
 
   def age_checking
-    errors.add(:birthday, "U are too small, baby") unless
-    Time.now.utc.year - birthday.year > 15
+    errors.add(:birthday, "U are too small, baby") if birthday && (Time.now.utc.year - birthday.year) < 15
   end
 end
